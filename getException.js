90c4eb12478e85b1e4c492248f873861923e46fe
nanokozmos <ovuncuzun@gmail.com>
2021-02-11 20:19:04 +0000

    updating variable names

--- getException.js
+++ getException.js
@@ -5,38 +5,28 @@ let status = {
         { type: "Invalid", text: "123" },
         { type: "Invalid", text: "456" },
         { type: "Invalid", text: "789" }
-    ],
-    added: [{ type: "Rows Added", text: "O row added" }],
-    updated: [{ type: "Rows Updated", text: "7 row added" }]
+    ]
 }
 
 let status2 = {
     dupe: [
         { type: "Dupe", text: "12345" },
         { type: "Dupe", text: "67890" }
-    ],
-    added: [{ type: "Rows Added", text: "O row added" }],
-    updated: [{ type: "Rows Updated", text: "5 row added" }]
+    ]
 }
 
 
 function getException(status, type) {
-    let exceptionData = [];
-    let invalidItems = "";
+    let errorData = "";
     if (status[type] && status[type].length > 0) {
-        invalidItems = _.reduce(status[type], (result, value, key) => {
-            let invalidData = result + value.text;
+        errorData = _.reduce(status[type], (result, value, key) => {
+            let valueText = result + value.text;
             return parseInt(key) != status[type].length - 1 ?
-                invalidData + ", " : invalidData;
-        },
-            type + " id: "
-        )
+                valueText + ", " : valueText;
+        }, type + " id: ")
     }
-    if (invalidItems) {
-        exceptionData.push(invalidItems);
-    }
-    return exceptionData;
+    return errorData;
 }
 
-console.log(getException(status, "invalid")); // [ 'invalid id: 123, 456, 789' ]
-console.log(getException(status2, "dupe")); // [ 'dupe id: 12345, 67890' ]
+console.log(getException(status, "invalid")); // invalid id: 123, 456, 789
+console.log(getException(status2, "dupe")); // dupe id: 12345, 67890
